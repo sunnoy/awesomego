f42b0cce0f2bf3b8615596a6e79edfed107043d9
lirui <ttzhsz@163.com>
2021-04-14 19:55:41 +0800

    socket编程，客户端产生读写完成

--- onemod/net-test/ws/client/client.go
+++ onemod/net-test/ws/client/client.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	url2 "net/url"
+	"time"
 )
 
 func main() {
@@ -22,13 +23,34 @@ func main() {
 
 	defer c.Close()
 
-	for {
-		_, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			return
+	// 读消息
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read err", err)
+				break
+			} else {
+				log.Printf("recv is %s", message)
+			}
 		}
-		log.Printf("recv: %s", message)
+	}()
+
+	// 写消息
+	ticker := time.NewTicker(time.Second * 5)
+
+	for {
+		select {
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write err", err)
+			} else {
+				log.Println(t.String())
+			}
+
+		}
+
 	}
 
 }
